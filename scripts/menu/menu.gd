bb4c4bf1785edcdb6777846712c7ddad7f821081
Lennoard <lennoardrai@gmail.com>
2022-05-23 01:02:50 -0300

    added menu soud effect

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -5,10 +5,12 @@ extends Control
 # var a = 2
 # var b = "text"
 
+var firstFocus = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer/StartButton.grab_focus()
+	firstFocus = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,8 +19,23 @@ func _ready():
 
 
 func _on_StartButton_pressed():
+	$MenuConfirm.play()
 	get_tree().change_scene("res://scenes/spaceship/spacecombat1.tscn")
 
+func _on_StartButton_focus_entered():
+	if (firstFocus):
+		return
+	$MenuSelect.play()
 
+func _on_StartButton_mouse_entered():
+	$VBoxContainer/StartButton.grab_focus()
+	
 func _on_ExitButton_pressed():
+	$MenuSelect.play()
 	get_tree().quit()
+
+func _on_ExitButton_focus_entered():
+	$MenuSelect.play()
+
+func _on_ExitButton_mouse_entered():
+	$VBoxContainer/ExitButton.grab_focus()
