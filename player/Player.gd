a588282d30ea6723749f98c839c90b04d173747e
Lennoard Silva <lennoardrai@gmail.com>
2022-05-25 07:44:34 -0300

    Update Player.gd

--- player/Player.gd
+++ player/Player.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
 export var speed: float = 1024
-export var fireDelay: float = 0.16
+export var fireDelay: float = 0.15
 
 var velocity := Vector2(0, 0)
 var shootType = 1 
 
-onready var bullet_instance = preload("res://bullet/Bullet.tscn") #instancia do projetil
+onready var bullet_instance = preload("res://bullet/Bullet.tscn") # instancia do projetil
 
 onready var fireDelayTimer := $FireDelay
 onready var shootPositions := $ShootPositions
